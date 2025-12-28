50e3e1e6898ab685c0f8959705048fcb506f22b3
Ajesh Anil <ajeshanil@gmail.com>
2025-12-28 19:01:59 +0530

    printing log

--- src/main/java/com/sector/webhook/controller/WebhookController.java
+++ src/main/java/com/sector/webhook/controller/WebhookController.java
@@ -1,6 +1,9 @@
 package com.sector.webhook.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sector.webhook.service.MessageStore;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,9 @@ public class WebhookController {
         this.store = store;
     }
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @GetMapping("/webhook")
     public ResponseEntity<String> verify(
             @RequestParam("hub.mode") String mode,
@@ -22,6 +28,7 @@ public class WebhookController {
             @RequestParam("hub.challenge") String challenge) {
 
         if ("subscribe".equals(mode) && VERIFY_TOKEN.equals(token)) {
+            System.out.println("Subscribing to hub  - " +  mode + " - "+ challenge);
             return ResponseEntity.ok(challenge);
         }
         return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
@@ -29,6 +36,13 @@ public class WebhookController {
 
     @PostMapping("/webhook")
     public ResponseEntity<Void> receive(@RequestBody String payload) {
+        System.out.println("Payload : \n" +  payload);
+        try {
+            Object jsonObject = objectMapper.readValue(payload, Object.class);
+            payload = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonObject);
+        } catch (JsonProcessingException e) {
+            System.out.println("Error converting object to json");
+        }
         store.add(payload);
         return ResponseEntity.ok().build();
     }
